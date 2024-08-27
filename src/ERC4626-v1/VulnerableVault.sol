be15c1a5b97690508fce07f9ce8aa764709cab38
BlockChomper <124448454+BlockChomper@users.noreply.github.com>
2024-08-27 21:39:23 +0530

    implement erc4626 fully, updating logging in exploit

--- src/ERC4626-v1/VulnerableVault.sol
+++ src/ERC4626-v1/VulnerableVault.sol
@@ -5,74 +5,110 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
-contract VulnerableVault is ERC20 {
+contract VulnerableVault is ERC20, IERC4626 {
     using SafeERC20 for IERC20;
     using Math for uint256;
 
-    IERC20 public immutable asset;
+    IERC20 private immutable _asset;
 
-    event Deposit(address indexed caller, address indexed owner, uint256 assets, uint256 shares);
-    event Withdraw(address indexed caller, address indexed receiver, address indexed owner, uint256 assets, uint256 shares);
-
-    constructor(IERC20 _asset) ERC20("Vulnerable Vault", "vVLT") {
-        asset = _asset;
+    constructor(IERC20 assetToken) ERC20("Vulnerable Vault", "vVLT") {
+        _asset = assetToken;
     }
 
-    function deposit(uint256 assets, address receiver) public virtual returns (uint256 shares) {
-        require(assets <= maxDeposit(receiver), "ERC4626: deposit more than max");
+    function asset() public view virtual override returns (address) {
+        return address(_asset);
+    }
 
-        shares = previewDeposit(assets);
-        
-        asset.safeTransferFrom(msg.sender, address(this), assets);
+    function deposit(uint256 assets, address receiver) public virtual override returns (uint256 shares) {
+        uint256 supply = totalSupply();
+        shares = supply == 0 ? assets : (assets * supply) / totalAssets();
         _mint(receiver, shares);
-
+        _asset.safeTransferFrom(msg.sender, address(this), assets);
         emit Deposit(msg.sender, receiver, assets, shares);
     }
 
-    function withdraw(uint256 assets, address receiver, address owner) public virtual returns (uint256 shares) {
-        require(assets <= maxWithdraw(owner), "ERC4626: withdraw more than max");
+    function mint(uint256 shares, address receiver) public virtual override returns (uint256 assets) {
+        assets = convertToAssets(shares);
+        _mint(receiver, shares);
+        _asset.safeTransferFrom(msg.sender, address(this), assets);
+        emit Deposit(msg.sender, receiver, assets, shares);
+    }
 
-        shares = previewWithdraw(assets);
-        
+    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256 shares) {
+    shares = balanceOf(owner);
+    uint256 actualAssets = (assets > convertToAssets(shares)) ? convertToAssets(shares) : assets;
+    _burn(owner, shares);
+    _asset.safeTransfer(receiver, actualAssets);
+    emit Withdraw(msg.sender, receiver, owner, actualAssets, shares);
+    return shares;
+}
+
+    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256 assets) {
         if (msg.sender != owner) {
             _spendAllowance(owner, msg.sender, shares);
         }
-
+        assets = convertToAssets(shares);
         _burn(owner, shares);
-        asset.safeTransfer(receiver, assets);
-
+        _asset.safeTransfer(receiver, assets);
         emit Withdraw(msg.sender, receiver, owner, assets, shares);
     }
 
-    function previewDeposit(uint256 assets) public view virtual returns (uint256) {
-        return assets;
+    function totalAssets() public view virtual override returns (uint256) {
+        return _asset.balanceOf(address(this));
     }
 
-    function previewWithdraw(uint256 assets) public view virtual returns (uint256) {
-        uint256 supply = totalSupply();
-        return supply == 0 ? assets : assets.mulDiv(supply, totalAssets(), Math.Rounding.Ceil);
-    }
-
-    function maxDeposit(address) public view virtual returns (uint256) {
-        return type(uint256).max;
-    }
-
-    function maxWithdraw(address owner) public view virtual returns (uint256) {
-        return convertToAssets(balanceOf(owner));
-    }
-
-    function totalAssets() public view virtual returns (uint256) {
-        return asset.balanceOf(address(this));
-    }
-
-    function convertToShares(uint256 assets) public view virtual returns (uint256) {
+    function convertToShares(uint256 assets) public view virtual override returns (uint256) {
         uint256 supply = totalSupply();
         return supply == 0 ? assets : assets.mulDiv(supply, totalAssets(), Math.Rounding.Floor);
     }
 
-    function convertToAssets(uint256 shares) public view virtual returns (uint256) {
+    function donateAssets(uint256 amount) public {
+        _asset.safeTransferFrom(msg.sender, address(this), amount);
+        // Deliberately not minting any shares for donated assets
+    }
+
+     function convertToAssets(uint256 shares) public view virtual override returns (uint256) {
         uint256 supply = totalSupply();
-        return supply == 0 ? shares : shares.mulDiv(totalAssets(), supply, Math.Rounding.Floor);
+        if (supply == 0) {
+            return shares;
+        }
+        // Vulnerable implementation: artificially inflates asset value of shares
+        return (shares * totalAssets() * 100) / supply;
+    }
+
+    function previewDeposit(uint256 assets) public view virtual override returns (uint256) {
+        return convertToShares(assets);
+    }
+
+    function previewMint(uint256 shares) public view virtual override returns (uint256) {
+        return convertToAssets(shares);
+    }
+
+    function previewWithdraw(uint256 assets) public view virtual override returns (uint256) {
+        return convertToShares(assets);
+    }
+
+    function previewRedeem(uint256 shares) public view virtual override returns (uint256) {
+        return convertToAssets(shares);
+    }
+
+    function maxDeposit(address) public view virtual override returns (uint256) {
+        return type(uint256).max;
+    }
+
+    function maxMint(address) public view virtual override returns (uint256) {
+        return type(uint256).max;
+    }
+
+    function maxWithdraw(address owner) public view virtual override returns (uint256) {
+        return convertToAssets(balanceOf(owner));
+    }
+
+    function maxRedeem(address owner) public view virtual override returns (uint256) {
+        return balanceOf(owner);
     }
 }
+
+
